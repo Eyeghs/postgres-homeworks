8112f4eca44f5de445a1f761b0b50ba79af41483
Ivan Shokhodko <pnupplol@gmail.com>
2024-10-23 10:47:15 +0300

    Добавлен основной функционал для main.py (загрузка данных из csv файла)

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,22 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2
+
+
+with psycopg2.connect(host='localhost', port=5432, user='postgres', password='12345', database='north') as conn:
+    with conn.cursor() as cur:
+        with open('north_data/customers_data.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", row)
+        with open('north_data/employees_data.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", row)
+        with open('north_data/orders_data.csv', 'r') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            next(csv_reader)
+            for row in csv_reader:
+                cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", row)
+        conn.commit()
